0b0c97b5ab1f55da854282502182b7e28323866c
ruby <rubyf2e@gmail.com>
2025-07-28 10:35:04 +0800

    調整 App.js 中的 import 語句，移除不必要的 useState 引入，並格式化 document.title 設定

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import ChatRoom from "./components/ChatRoom";
 import "./scss/style.scss";
 
 function App() {
   useEffect(() => {
-    document.title = process.env.REACT_APP_TITLE || "AI PDF 小幫手 - 智能PDF分析助手";
+    document.title =
+      process.env.REACT_APP_TITLE || "AI PDF 小幫手 - 智能PDF分析助手";
   }, []);
 
   return (
